d14634f936fe5bc76a0e96b9c974838a78a640c3
Ashish <ash1174@gmail.com>
2017-11-24 13:08:16 +0530

    Update BaseController.java

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -22,7 +22,6 @@ public class BaseController {
 	
 	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	Date date = new Date();
-	System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
